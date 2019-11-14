094e913add115bc8b6c4a47f943cc0cf59768cca
Kelvin Cortez <kelvincortez@Kelvins-iMac.local>
2019-11-14 19:18:01 +1300

    End of Section 16

--- ConnectApp.API/Migrations/20191108022240_MessageEntityAdded.Designer.cs
+++ ConnectApp.API/Migrations/20191108022240_MessageEntityAdded.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ConnectApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ConnectApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20191108022240_MessageEntityAdded")]
+    partial class MessageEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
